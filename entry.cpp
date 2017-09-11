d5ab25787766600dd3d3dba6419d7a8580b40f84
jy04916345 <gloomweihan@gmail.com>
2017-09-11 13:08:46 +0800

    add entry point

---
+++ entry.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+#ifdef _MSC_VER
+int main(int argc, char* argv[])
+#endif
+{
+	cout << "This is LightingGE entry." << endl;
+	system("pause");
+	return 0;
+}
