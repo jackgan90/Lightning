a61472b8237cc17f0edc360f78840ebca3827e7b
GalaxyRipple <gloomweihan@gmail.com>
2017-10-11 01:27:25 +0800

    upload temp version

--- renderer/d3d12/d3d12device.h
+++ renderer/d3d12/d3d12device.h
@@ -4,23 +4,24 @@
 #include <vector>
 #include <memory>
 #include "idevice.h"
-#include "d3d12rendercontext.h"
+#include "filesystem.h"
+#include "d3d12shadermanager.h"
 
 namespace LightningGE
 {
 	namespace Renderer
 	{
 		using Microsoft::WRL::ComPtr;
+		using Foundation::FileSystemPtr;
 		class LIGHTNINGGE_RENDERER_API D3D12Device : public IDevice
 		{
 		public:
 			friend class D3D12RenderContext;
 			friend class D3D12Renderer;
 			friend class D3D12SwapChain;
-			D3D12Device(ComPtr<ID3D12Device> pDevice, D3D12RenderContext* pContext);
+			D3D12Device(ComPtr<ID3D12Device> pDevice, const FileSystemPtr& fs);
 			~D3D12Device()override;
 			void ClearRenderTarget(const RenderTargetPtr& rt, const ColorF& color, const RectI* pRects = nullptr, const int rectCount = 0)override;
-			void ReleaseRenderResources()override;
 			BlendStatePtr CreateBlendState()override;
 			DepthStencilStatePtr CreateDepthStencilState()override;
 			PipelineStateObjectPtr CreatePipelineStateObject()override;
@@ -28,7 +29,8 @@ namespace LightningGE
 			ShaderPtr CreateShader(ShaderType type, const std::string& shaderName, const ShaderDefine& defineMap)override;
 			RasterizerStatePtr CreateRasterizerState()override;
 		private:
-			D3D12RenderContext* m_context;
+			FileSystemPtr m_fs;
+			std::shared_ptr<D3D12ShaderManager> m_shaderMgr;
 			ComPtr<ID3D12Device> m_device;
 			ComPtr<ID3D12CommandQueue> m_commandQueue;
 			std::vector<ComPtr<ID3D12CommandAllocator>> m_commandAllocators;
