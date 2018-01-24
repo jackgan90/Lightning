7df25f8273a1cee8001714fdb7add8055b2ff605
jackgan90 <ganjiework@163.com>
2018-01-24 14:42:48 +0800

    MOD:add operator overload

--- render/types/matrix.h
+++ render/types/matrix.h
@@ -5,31 +5,44 @@ namespace LightningGE
 {
 	namespace Render
 	{
-		template<typename _Scalar, int Rows, int Columns, bool RowMajor=true>
+		template<typename _Scalar, int Rows, int Columns>
 		class Matrix
 		{
 		public:
 			EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 			Matrix();
-			void TransposeInPlace()
+			Matrix(const _Scalar* data, bool rowMajor) { Set(data, rowMajor); }
+			bool operator==(const Matrix<_Scalar, Rows, Columns>& m){return m_value == m.m_value;}
+			bool operator!=(const Matrix<_Scalar, Rows, Columns>& m){return !(*this == m); }
+			Matrix<_Scalar, Rows, Columns> operator+(const Matrix<_Scalar, Rows, Columns>& m)const
 			{
-				m_value.transposeInPlace();
+				Matrix<_Scalar, Rows, Columns> result(*this);
+				result.m_value += m.m_value;
+				return result;
 			}
-			Matrix<_Scalar, Columns, Rows> Transpose()const
+			Matrix<_Scalar, Rows, Columns> operator-(const Matrix<_Scalar, Rows, Columns>& m)const
 			{
-				return m_value.transpose();
+				Matrix<_Scalar, Rows, Columns> result(*this);
+				result.m_value -= m.m_value;
+				return result;
 			}
-			void Set(const _Scalar* data);
-			const _Scalar operator()(const int row, const int column)
+			template<int Dimension>
+			Matrix<_Scalar, Rows, Dimension> operator*(const Matrix<_Scalar, Columns, Dimension>& m)const
 			{
-				return m_value(row, column);
+				Matrix<_Scalar, Rows, Dimension> result(*this);
+				result.m_value *= m.m_value;
+				return result;
 			}
+			void TransposeInPlace() { m_value.transposeInPlace();}
+			Matrix<_Scalar, Columns, Rows> Transpose()const { return m_value.transpose(); }
+			void Set(const _Scalar* data, bool rowMajor);
+			const _Scalar operator()(const int row, const int column) { return m_value(row, column); }
 		private:
 			Eigen::Matrix<_Scalar, Rows, Columns> m_value;
 		};
 
-		template<typename _Scalar, int Rows, int Columns, bool RowMajor>
-		Matrix<_Scalar, Rows, Columns, RowMajor>::Matrix()
+		template<typename _Scalar, int Rows, int Columns>
+		Matrix<_Scalar, Rows, Columns>::Matrix()
 		{
 			for (std::size_t i = 0;i < Rows;++i)
 			{
@@ -40,11 +53,11 @@ namespace LightningGE
 			}
 		}
 
-		template<typename _Scalar, int Rows, int Columns, bool RowMajor>
-		void Matrix<_Scalar, Rows, Columns, RowMajor>::Set(const _Scalar* data)
+		template<typename _Scalar, int Rows, int Columns>
+		void Matrix<_Scalar, Rows, Columns>::Set(const _Scalar* data, bool rowMajor)
 		{
 			int current = 0;
-			if (RowMajor)
+			if (rowMajor)
 			{
 				for (int i = 0;i < Rows;++i)
 				{
--- unittest/mathtest.cpp
+++ unittest/mathtest.cpp
@@ -17,7 +17,6 @@ namespace
 			}
 			out << std::endl;
 		}
-		out << std::endl;
 		return out;
 	}
 	TEST_CASE("Matrix function test", "[Matrix Test]")
@@ -26,15 +25,41 @@ namespace
 		std::cout << "Original matrix is :" << std::endl;
 		std::cout << m;
 		float data[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-		m.Set(data);
+		m.Set(data, false);
+		Matrix<float, 4, 4> m1 = m;
 
 		std::cout << "After Set the matrix becomes :" << std::endl;
-		std::cout << m;
+		std::cout << m << std::endl;
+
+		std::cout << "Constructed from copy constructor:" << std::endl;
+		std::cout << m1 << std::endl;
+
+		std::cout << "m == m1:" << (m == m1) << std::endl;
+		std::cout << "m != m1:" << (m != m1) << std::endl;
+
+		Matrix<float, 4, 4> m2 = m + m1;
+		std::cout << "m2 is " << std::endl;
+		std::cout << m2 << std::endl;
+
+		m2 = m1;
+		std::cout << "After assignment m2 is " << std::endl;
+		std::cout << m2 << std::endl;
+
+		auto m3 = m2 - m1;
+		std::cout << "m3 = m2 - m1" << std::endl;
+		std::cout << m3 << std::endl;
+
+		std::cout << "m1 is " << std::endl;
+		std::cout << m1 << std::endl;
+		std::cout << "m2 is " << std::endl;
+		std::cout << m2 << std::endl;
+		std::cout << "m1 * m2 is " << std::endl;
+		std::cout << m1 * m2 << std::endl;
 
 		m.TransposeInPlace();
 
 		std::cout << "After transposeInPlace, the matrix becomes:" << std::endl;
-		std::cout << m;
+		std::cout << m << std::endl;
 		system("pause");
 	}
 }
