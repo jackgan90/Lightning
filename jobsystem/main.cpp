1bb31f7c27ac60b462f461fcd06f6652d036ccb1
GalaxyRipple <ganjiework@163.com>
2018-03-27 01:04:43 +0800

    MOD:add job pressure test

--- jobsystem/main.cpp
+++ jobsystem/main.cpp
@@ -4,14 +4,28 @@
 
 using JobSystem::JobManager;
 using JobSystem::JobType;
+using JobSystem::IJob;
 
 std::mutex mutex;
 
 
-void calc_sum(int a, int b)
+void job_spawn(std::uint64_t currentJob, std::uint64_t jobCount)
 {
-	int c = a + b;
-	std::cout << "Running in thread:" << std::this_thread::get_id() << " equation:" << a + " + " << b << " = " << c << std::endl;
+	std::cout << "Running in thread:" << std::this_thread::get_id() << "current job:" << currentJob << ", jobCount" << jobCount << std::endl;
+	IJob * job{ nullptr };
+	JobType type = JobType::SHORT_TERM;
+	//if (currentJob % 2)
+	//	type = JobType::LONG_TERM;
+	if(currentJob < jobCount)
+	{
+		job = JobManager::Instance().AllocateJob(type, nullptr, job_spawn, currentJob + 1, jobCount);
+	}
+	else
+	{
+		std::uint64_t nextLayerJobCount = jobCount == static_cast<std::uint64_t>(-1) ? jobCount : jobCount + 1;
+		job = JobManager::Instance().AllocateJob(type, nullptr, job_spawn, 0, nextLayerJobCount);
+	}
+	JobManager::Instance().RunJob(job);
 }
 
 void task_generation_job()
@@ -21,16 +35,15 @@ void task_generation_job()
 	jobs.push_back(masterJob);
 	for (int i = 0;i < 100;i++)
 	{
-		std::cout << "Add job " << (i + 1) << " calculating " << (2 * i) << " + " << ( 5 * i - 10) << std::endl;
-		auto job = JobManager::Instance().AllocateJob(JobType::SHORT_TERM, masterJob, calc_sum, 2 * i, 5 * i - 10);
+		auto job = JobManager::Instance().AllocateJob(JobType::SHORT_TERM, masterJob, job_spawn, 0, 2);
 		jobs.push_back(job);
 	}
 	for (auto job : jobs)
 		JobManager::Instance().RunJob(job);
 	JobManager::Instance().WaitForCompletion(masterJob);
 	std::cout << "All calculation job done!shutting up job system!" << std::endl;
-	JobManager::Instance().ShutDown();
-	std::cout << "JobSystem shut down!" << std::endl;
+	//JobManager::Instance().ShutDown();
+	//std::cout << "JobSystem shut down!" << std::endl;
 }
 
 void hello()
