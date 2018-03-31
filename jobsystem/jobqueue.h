ee986a0838482899cbadce2ba9f60ba9b1cf3870
GalaxyRipple <ganjiework@163.com>
2018-03-31 22:34:41 +0800

    MOD:job system changes to headers only

--- jobsystem/jobqueue.h
+++ jobsystem/jobqueue.h
@@ -7,11 +7,26 @@ namespace JobSystem
 	class JobQueue
 	{
 	public:
-		JobQueue(std::size_t size=GLOBAL_JOB_QUEUE_SIZE);
+		JobQueue::JobQueue(std::size_t size = GLOBAL_JOB_QUEUE_SIZE):m_queue(size) {}
 		JobQueue(const JobQueue&) = delete;
 		JobQueue& operator=(const JobQueue&) = delete;
-		void Push(IJob* job);
-		IJob* Pop();
+		void Push(IJob* job)
+		{
+			auto res = m_queue.enqueue(job);
+			assert(res);
+		}
+		IJob* Pop()
+		{
+			if (m_queue.size_approx() > 0)
+			{
+				IJob* job{ nullptr };
+				auto success = m_queue.try_dequeue(job);
+				if (!success)
+					return nullptr;
+				return job;
+			}
+			return nullptr;
+		}
 	private:
 		static constexpr std::size_t GLOBAL_JOB_QUEUE_SIZE{ 8192 };
 		moodycamel::ConcurrentQueue<IJob*> m_queue;
