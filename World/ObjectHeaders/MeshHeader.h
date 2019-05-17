a367c778a1249c30fbcae8e970c1ef6f5f3c6f89
gjn1754 <gjn1754@corp.netease.com>
2019-05-17 20:03:26 +0800

    Design MeshHeader data structure.

--- World/ObjectHeaders/MeshHeader.h
+++ World/ObjectHeaders/MeshHeader.h
@@ -6,13 +6,30 @@ namespace Lightning
 {
 	namespace World
 	{
+		//Maximum number of vertex streams.Note position/normal/tangent/bitangent also accounts to stream count.
+		//eg.Position + Normal + Tangents + Bitangents + Other vertex attributes cannot exceed MaxVertexStreams.
+		constexpr std::uint8_t MaxVertexStreams = 8u;
+		struct VertexStream
+		{
+			std::uint8_t Components;
+			std::uint32_t Offset;
+			std::uint32_t Count;
+		};
+
+		static_assert(std::is_pod<VertexStream>::value, "VertexStream must be a POD type.");
+
 		struct MeshHeader
 		{
 			std::uint32_t Magic;
-			std::uint8_t VertexAttributes;		//number of vertex attributes
-			std::uint64_t PositionOffset;		//point to the start of vertex position data,measured in offset from file begin
-			std::uint64_t NormalOffset;			//point to the start of normal data, measured in offset from file begin, 0 if not present
-			std::uint64_t TangentOffset;		//point to the start of tangent data, measured in offset from file begin, 0 if not present
+			std::uint8_t VertexStreams;		//number of vertex streams
+			std::uint8_t IndexSize;			
+			std::uint32_t IndexOffset;
+			std::uint32_t IndexCount;
+			VertexStream Positions;			//point to the start of vertex position data,measured in offset from file begin
+			VertexStream Normals;			//point to the start of normal data, measured in offset from file begin, 0 if not present
+			VertexStream Tangents;			//point to the start of tangent data, measured in offset from file begin, 0 if not present
+			VertexStream Bitagents;			
+			VertexStream VertexAttributes[MaxVertexStreams];
 		};
 		
 		static_assert(std::is_pod<MeshHeader>::value, "MeshHeader must be a POD type.");
